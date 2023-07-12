85d0f9ff3d40eccbbf4c749115eb48d1cf45badf
ZsGyao <98084156+ZsGyao@users.noreply.github.com>
2023-07-12 14:38:49 +0800

    Update InventoryPanel.cpp

--- Source/InventorySystem/Private/InventoryPanel.cpp
+++ Source/InventorySystem/Private/InventoryPanel.cpp
@@ -96,3 +96,95 @@ bool UInventoryPanel::NativeOnDrop(const FGeometry& InGeometry, const FDragDropE
 	}
 	return false;
 }
+
+FReply UInventoryPanel::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	auto Reply = Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+	UE_LOG(LogTemp, Warning, TEXT("Into InventoryPanel NativeOnMouseButtonDown"));
+
+	// 如果鼠标按下的位置在InventoryPanel内
+	if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Start Dragging, the pisition from Butten down"));
+		// 按下确定拖拽
+		bIsDragging = true;
+
+		return FReply::Handled();
+	}
+
+	return  FReply::Unhandled();
+}
+
+FReply UInventoryPanel::NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	FReply Reply = Super::NativeOnMouseButtonUp(InGeometry, InMouseEvent);
+
+	UE_LOG(LogTemp, Warning, TEXT("Into Inventory NativeOnMouseButton UP UP UP"));
+
+	if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+	{
+		 //UE_LOG(LogTemp, Warning, TEXT("NativeOnMouseButtonUp, the Mouse Butten will up soon"));
+
+		bIsDragging = false;
+		UE_LOG(LogTemp, Warning, TEXT("Stop Dragging,the bIsDragging is: {%d} "), bIsDragging);
+		Reply = FReply::Handled();
+	}
+
+	return Reply;
+}
+
+FReply UInventoryPanel::NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	// todo 只有在InventoryPanel中才更新坐标？
+	FVector2D MousePosition = FVector2D();
+	GetCursorPosition(MousePosition);
+
+	if (bIsDragging && this->IsHovered())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Into Main Menu NativeOnMouseMove, bIsDragging is {%d};"), bIsDragging);
+
+		UpdateDragging();
+		return FReply::Handled();
+	 }
+	return FReply::Unhandled();
+}
+
+void UInventoryPanel::GetCursorPosition(FVector2D& MousePosition) const
+{
+	APlayerController* pc = GetWorld()->GetFirstPlayerController();
+	if (pc)
+	{
+		ULocalPlayer* LocalPlayer = pc->GetLocalPlayer();
+		if (LocalPlayer && LocalPlayer->ViewportClient)
+		{
+			if (LocalPlayer->ViewportClient->GetMousePosition(MousePosition))
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Cursor.position X :  %f , Y : %f"), MousePosition.X, MousePosition.Y);
+			}
+		}
+	}
+}
+
+void UInventoryPanel::UpdateDragging()
+{
+	FVector2D MousePosition = FVector2D();
+	GetCursorPosition(MousePosition);
+	if(!bIsInViewpoint)
+	{
+		this->RemoveFromParent();
+		this->AddToViewport(300);
+		bIsInViewpoint = true;
+	}
+
+	this->SetPositionInViewport(MousePosition);
+	//this->SetPositionInViewport(PlayerCharacter->CursorMousePosition);
+	UE_LOG(LogTemp, Warning, TEXT("SetPositionInViewport in X: {%f}, Y: {%f}"), PlayerCharacter->CursorMousePosition.X, PlayerCharacter->CursorMousePosition.Y);
+}
+
+void UInventoryPanel::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent,
+	UDragDropOperation*& OutOperation)
+{
+	Super::NativeOnDragDetected(InGeometry, InMouseEvent, OutOperation);
+
+
+}
